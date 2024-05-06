762a8a564238465708a9e751ab83ebf9e9fcc963
indrichetrwo00 <masakonakamura00@outlook.com>
2024-05-06 09:25:07 +0700

    Create c-o-m-i-c-w-a-l-k-e-r.meta.js

---
+++ c-o-m-i-c-w-a-l-k-e-r.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         c-o-m-i-c-w-a-l-k-e-r
+// @namespace    https://github.com/indrichetrwo00/o_vien_phuong/raw/main/c-o-m-i-c-w-a-l-k-e-r.user.js
+// @version      2024.03.27
+// @description  down c-o-m-i-c-w-a-l-k-e-r
+// @author       You
+// @include      /^https:\/\/c[o0]mic\-w[a@]lker\.c[o0]m\x2Fd[e0]tail\x2F.+\x2Fe[p0]isodes\x2F.+/
+// @icon         https://raw.githubusercontent.com/indrichetrwo00/o_vien_phuong/main/c-o-m-i-c-w-a-l-k-e-r.png
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/indrichetrwo00/o_vien_phuong/raw/main/c-o-m-i-c-w-a-l-k-e-r.user.js
+// @updateURL    https://github.com/indrichetrwo00/o_vien_phuong/raw/main/c-o-m-i-c-w-a-l-k-e-r.meta.js
+// ==/UserScript==
